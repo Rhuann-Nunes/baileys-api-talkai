9ec8a5a57a05c3b9bcc5bfcb6a29020e362008a9
Rhuann-Nunes <rhuann.nunes@tecpav.com.br>
2025-02-19 21:07:52 -0300

    fix: update build and deployment configuration

---
+++ paths.js
@@ -0,0 +1,10 @@
+const tsConfigPaths = require('tsconfig-paths');
+const tsConfig = require('./tsconfig.json');
+
+const baseUrl = './dist';
+tsConfigPaths.register({
+    baseUrl,
+    paths: {
+        '@/*': ['*']
+    },
+}); 
